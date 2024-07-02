57a81685c123478110c53bc142a338d395aae1fe
DevRodox <rodrigo17.ed@gmail.com>
2024-07-02 13:36:47 -0600

    Add JournalLayout

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,9 +1,13 @@
 import { Typography } from "@mui/material";
+import { JournalLayout } from "../layout/JournalLayout";
 
 export const JournalPage = () => {
     return(
-        <>
-            <Typography variant={'h1'}>JournalPage</Typography>
-        </>
-    );
+        <JournalLayout > 
+            <Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae debitis deserunt, iure in excepturi optio amet eos deleniti laborum vitae cum similique unde voluptatibus. Aliquid tempore hic necessitatibus minus vero.</Typography>
+        
+            {/* Nothing selected */}
+            {/* Note page */}
+        </JournalLayout>        
+);
 };
