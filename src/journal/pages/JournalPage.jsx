9416c23174bb1a3901e43eef740aa81099c570a3
DevRodox <rodrigo17.ed@gmail.com>
2024-07-03 13:01:19 -0600

    Add NothingSelecteedView

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,13 +1,13 @@
-import { Typography } from "@mui/material";
 import { JournalLayout } from "../layout/JournalLayout";
+import { NothingSelectedView } from "../views";
 
 export const JournalPage = () => {
     return(
         <JournalLayout > 
-            <Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae debitis deserunt, iure in excepturi optio amet eos deleniti laborum vitae cum similique unde voluptatibus. Aliquid tempore hic necessitatibus minus vero.</Typography>
-        
-            {/* Nothing selected */}
+
+            <NothingSelectedView />
             {/* Note page */}
+
         </JournalLayout>        
 );
 };
