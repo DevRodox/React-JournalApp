e0194c3e3c5dc4b8a5b2e1cb69fa9551dfc8cc52
DevRodox <rodrigo17.ed@gmail.com>
2024-10-07 16:36:38 -0600

    Add Store main

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { JournalApp } from './JournalApp'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { JournalApp } from './JournalApp';
+import { Provider } from 'react-redux';
+import { store } from './store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <JournalApp/>
+    <Provider store={ store }>
+      <JournalApp/>
+    </Provider>
   </React.StrictMode>,
 )
