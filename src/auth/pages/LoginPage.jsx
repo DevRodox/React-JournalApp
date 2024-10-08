770cd9c9072a2f03ed6330ea660c9abdc9fe7649
DevRodox <rodrigo17.ed@gmail.com>
2024-10-08 02:03:45 -0600

    handle login form

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,12 +1,35 @@
+import { useDispatch } from 'react-redux';
 import { Link as RouterLink } from 'react-router-dom';
-import { Google } from '@mui/icons-material';
+import { Google, Password } from '@mui/icons-material';
 import { Button, Grid, Link, TextField, Typography } from '@mui/material';
+
 import { AuthLayout } from '../layout/AuthLayout';
+import { useForm } from '../../hooks/useForm';
+import { checkingAuthentication, startGoogleSignIn } from '../../store';
 
 export const LoginPage = () => {
+
+    const dispatch = useDispatch();
+
+    const { email, password, onInputChange  } = useForm({
+        email: '',
+        password: ''
+    });
+
+    const onSubmit = ( e ) => {
+        e.preventDefault();
+        console.log({ email, password });
+        dispatch( checkingAuthentication() );
+    };
+
+    const onGoogleSignIn = () => {
+        console.log('onGoogleSignIn');
+        dispatch( startGoogleSignIn() );
+    };
+
     return(
         <AuthLayout title='Login'>
-                <form>
+                <form onSubmit={ onSubmit }>
 
                     <Grid container>
 
@@ -16,7 +39,10 @@ export const LoginPage = () => {
                                 type='email' 
                                 placeholder='correo@google.com'
                                 fullWidth
-                            />
+                                name='email'
+                                value={ email }
+                                onChange={ onInputChange }
+                                />
                         </Grid>
 
                         <Grid item xs={ 12 } sx={{ mt: 2 }}>
@@ -25,17 +51,23 @@ export const LoginPage = () => {
                                 type='password' 
                                 placeholder='Contraseña'
                                 fullWidth
-                            />
+                                name='password'
+                                value={ password }
+                                onChange={ onInputChange }
+                                />
                         </Grid>
 
                         <Grid container spacing={ 2 } sx={{ mb: 2, mt: 1 }}>
                             <Grid item xs={ 12 } sm={ 6 }>
-                                <Button variant='contained' fullWidth>
+                                <Button type='submit' variant='contained' fullWidth>
                                     Login
                                 </Button>
                             </Grid>
                             <Grid item xs={ 12 } sm={ 6 }>
-                                <Button variant='contained' fullWidth>
+                                <Button  
+                                    variant='contained'
+                                    fullWidth
+                                    onClick={ onGoogleSignIn }>
                                     <Google />
                                     <Typography sx={{ ml: 1 }}>Google</Typography>
                                 </Button>
---
+++ src/store/auth/thunks.js
@@ -0,0 +1,13 @@
+import { chekingCredentials } from './';
+
+export const checkingAuthentication = ( email, password ) => {
+    return async( dispatch ) => {
+        dispatch( chekingCredentials() );
+    };
+};
+
+export const startGoogleSignIn = () => {
+    return async( dispatch ) => {
+        dispatch( chekingCredentials() );
+    };
+};
